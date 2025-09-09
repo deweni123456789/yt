7a3dc192cd521ed05c2e3bf0ba27b66ee5aac4af
deweni123456789 <schoford91@gmail.com>
2025-09-09 11:40:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from modules.youtube import detect_platform, download_and_send
 
-API_ID = int(os.getenv("API_ID", "123456"))   # set in Railway
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "5047271"))   # set in Railway
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7896090354:AAE_NaVu_d-x-TCJt9CPNMl9t94Mltw_jrw")
 
 app = Client(
     "yt_downloader_bot",
